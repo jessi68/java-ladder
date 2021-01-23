3937fd65929c5de41ffac64914f24d500a508c2f
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2021-01-23 18:44:18 +0900

    feat: 연결된 다리들을 갖고 있는 사다리 클래스 구현

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -1,5 +1,17 @@
 package ladder.domain;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Ladder {
 
+    private HashMap<Integer, Step> stepAt = new HashMap<>();
+
+    public Ladder(HashMap<Integer, Step> stepAt) {
+        this.stepAt = stepAt;
+    }
+
+    public Step getWithHeight(int height) {
+        return stepAt.get(height);
+    }
 }
---
+++ src/main/java/test/ladder/domain/LadderTest.java
@@ -0,0 +1,26 @@
+package test.ladder.domain; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Ladder Tester. 
+* 
+* @author jessi68
+* @since <pre>1월 23, 2021</pre> 
+* @version 1.0 
+*/ 
+public class LadderTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+@
+
+} 

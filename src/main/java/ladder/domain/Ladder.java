c758a7d0a508952675a4998093104970f9c60c62
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2021-01-23 19:41:06 +0900

    fit: Ladder 순서를 나타내는 변수 추가하고, 테스트와 변경을 수월하게 하기 위해, step 을 사다리에 추가하는 함수 구현

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -6,13 +6,18 @@ import java.util.List;
 public class Ladder {
 
     private HashMap<Integer, Step> stepsAt = new HashMap<>();
+    private int order;
 
-    public Ladder(HashMap<Integer, Step> stepAt) {
-        this.stepsAt = stepAt;
+    public Ladder(int order) {
+        this.order = order;
     }
 
     public Step getWithHeight(int height) {
         return stepsAt.get(height);
+    }
+
+    public void putStep(int height, Step step) {
+        this.stepsAt.put(height, step);
     }
 
     public HashMap<Integer, Step> getStepAt() {

83f942e6df51225ee1f2d7033b00b918b8d5f922
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2021-01-23 19:10:29 +0900

    feat: 사다리와 사다리를 연결하는 다리 클래스 생성 및, 다리를 통과했을때 나오는 결과 사다리를 반환하는 함수 구현

--- src/main/java/ladder/domain/Step.java
+++ src/main/java/ladder/domain/Step.java
@@ -1,2 +1,20 @@
-package ladder.domain;public class Bridge {
+package ladder.domain;
+
+public class Step {
+    private int height;
+    private Ladder left;
+    private Ladder right;
+
+    public Step(int height, Ladder left, Ladder right) {
+        this.height = height;
+        this.left = left;
+        this.right = right;
+    }
+
+    public Ladder passThrough(Ladder cur) {
+        if(left.equals(cur)) {
+            return right;
+        }
+        return left;
+    }
 }
